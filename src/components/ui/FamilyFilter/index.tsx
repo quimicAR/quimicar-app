c62410ff541ecc2eaa71baee318dc69efaa962a7
c0sta <gabriel.moura6@fatec.sp.gov.br>
2021-11-10 21:49:45 -0300

    feat: started using helper to handleBackground

--- src/components/ui/FamilyFilter/index.tsx
+++ src/components/ui/FamilyFilter/index.tsx
@@ -2,7 +2,7 @@
 import {Text, Box, Avatar} from 'native-base';
 import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
-import {formatCategory} from '../../../helpers/format-category';
+import {handleBackground} from '../../../helpers/handle-background';
 import api from '../../../services';
 import {FamilyFilterContainer} from './styles';
 
@@ -14,44 +14,24 @@ export const FamilyFilter: React.FC = () => {
     setFamily(data);
   };
 
-  const handleBackground = (item: string): string => {
-    const category = item.replace(' ', '').toLowerCase();
-    switch (formatCategory(category)) {
-      case 'alkalineEarthMetals'.toLowerCase():
-        return '#009cc7';
-      case 'alkaliMetals'.toLowerCase():
-        return '#fe1110';
-      case 'nobleGases'.toLowerCase():
-        return '#6f25a1';
-      case 'nonMetal'.toLowerCase():
-        return '#f5be25';
-      case 'transitionMetals'.toLowerCase():
-        return '#ff8000';
-      case 'lanthanoids':
-        return '#cf6142';
-      case 'actinoids':
-        return '#57733b';
-      case 'postTransitionMetals'.toLowerCase():
-        return '#006da4';
-      default:
-        return '#a1d344';
-    }
-  };
-
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <FamilyFilterContainer>
-      <Text mb={3} fontWeight="bold">
+      <Text mb={5} fontWeight="bold">
         Groups
       </Text>
       <FlatList<string>
         data={family}
         renderItem={({item}) => (
           <Box mr={3} alignItems="center">
-            <Avatar mb={1} backgroundColor={handleBackground(item)}>
+            <Avatar
+              mb={1}
+              backgroundColor={handleBackground(
+                item.replace(' ', '').toLowerCase(),
+              )}>
               {item.charAt(0).toUpperCase()}
             </Avatar>
             <Text fontSize="xs">{item}</Text>
