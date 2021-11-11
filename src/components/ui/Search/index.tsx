715bf9ac2e58e14032783a9fd42af9b067fb6aa5
c0sta <gabriel.moura6@fatec.sp.gov.br>
2021-11-10 21:44:15 -0300

    feat: search atom component

--- src/components/ui/Search/index.tsx
+++ src/components/ui/Search/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import {Input} from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {NativeSyntheticEvent, TextInputChangeEventData} from 'react-native';
 
-export const Search: React.FC = (): JSX.Element => {
+interface SearchProps {
+  value: string;
+  onChange: (event: NativeSyntheticEvent<TextInputChangeEventData>) => void;
+}
+
+export const Search: React.FC<SearchProps> = ({
+  onChange,
+  value,
+}): JSX.Element => {
   return (
     <Input
-      w="90%"
+      w="94%"
       h="12"
       m={3}
-      pr={5}
+      pr={10}
       variant="filled"
       placeholder="Search element..."
+      value={value}
+      onChange={onChange}
       InputRightElement={<Icon name="search" color="#333" size={25} />}
       _light={{
         placeholderTextColor: 'blueGray.400',
