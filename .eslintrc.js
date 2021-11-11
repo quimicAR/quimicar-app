97bac1a5c97b904c93217b5013036854dcfcfe8b
c0sta <gabriel.moura6@fatec.sp.gov.br>
2021-11-10 21:52:23 -0300

    feat: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
     'react-native/no-inline-styles': 'off',
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
