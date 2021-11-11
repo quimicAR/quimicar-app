18890ac2687a3f584e5a85643744c6a109344271
c0sta <gabriel.moura6@fatec.sp.gov.br>
2021-11-10 21:52:02 -0300

    feat: homeScreen with search implementation

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,13 +1,55 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {AtomsList, FamilyFilter, Search, Alert} from '../../components';
 import {Box} from 'native-base';
-import React from 'react';
-import {AtomsList, FamilyFilter, Search} from '../../components';
+import {IElement} from '../../interfaces';
+import {getAllElements} from '../../services/elements/get-all';
 
 const HomeScreen: React.FC = () => {
+  const [atomsData, setAtomsData] = useState<IElement[]>([]);
+  const [filteredAtoms, setFilteredAtoms] = useState<IElement[]>([]);
+  const [searchAtoms, setSearchAtoms] = useState('');
+
+  const loadElements = () => {
+    getAllElements()
+      .then(({data}) => {
+        const filtered = data.filter(atom => atom.enabled);
+        setAtomsData(filtered);
+        setFilteredAtoms(filtered);
+      })
+      .catch((error: any) => (
+        <Alert status="error" title="Error!" text={error} />
+      ));
+  };
+
+  const handleSearchAtoms = useCallback(() => {
+    if (searchAtoms.length >= 1)
+      setFilteredAtoms(
+        atomsData.filter(
+          atom =>
+            atom.name === searchAtoms ||
+            atom.number.toString() === searchAtoms ||
+            atom.symbol === searchAtoms,
+        ),
+      );
+    else loadElements();
+  }, [atomsData, searchAtoms]);
+
+  useEffect(() => {
+    loadElements();
+  }, []);
+
+  useEffect(() => {
+    handleSearchAtoms();
+  }, [searchAtoms]);
+
   return (
     <Box flex={1}>
-      <Search />
+      <Search
+        onChange={event => setSearchAtoms(event.nativeEvent.text)}
+        value={searchAtoms}
+      />
       <FamilyFilter />
-      <AtomsList />
+      <AtomsList data={filteredAtoms} />
     </Box>
   );
 };
